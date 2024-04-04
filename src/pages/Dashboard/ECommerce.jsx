1d78b9e07323178d4bc17f3588ba8813e8ce4f2c
react-seawind <rajan@seawind.in>
2024-04-04 12:53:30 +0530

    db changes

--- src/pages/Dashboard/ECommerce.jsx
+++ src/pages/Dashboard/ECommerce.jsx
@@ -76,10 +76,10 @@ const ECommerce = () => {
         <CardThree SubjectData={SubjectData} />
         <CardFour ChapterData={ChapterData} />
       </div>
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+      {/*<div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartThree />
-      </div>
+      </div>*/}
     </div>
   );
 };
